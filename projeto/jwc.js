329603558ea2115ea204009120cb24a31e73aaa6
Cauã Israel <caua.israel02@gmail.com>
2025-02-20 11:44:44 -0300

    20/02/ 11:44

---
+++ projeto/jwc.js
@@ -0,0 +1,17 @@
+let contador = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+nextImage();
+}, 3000)
+
+
+function nextImage(){
+    contador++;
+    if(contador>5){
+        contador = 1;
+    }
+
+    document.getElementById("radio"+contador).checked = true;
+
+}
