9b4867feae2b3f10258dc76554dfe4f40009900f
Cauã Israel <caua.israel02@gmail.com>
2025-02-26 20:31:10 -0300

    20:31

--- projeto/jwc.js
+++ projeto/jwc.js
@@ -15,3 +15,14 @@ function nextImage(){
     document.getElementById("radio"+contador).checked = true;
 
 }
+
+
+
+
+function exibirDados() {
+    //Captura os valores dos inputs
+    let email = document.getElementById('correio').value;
+
+    //Usando document.write para exibir os dados da página
+    document.write('<p><strong>E-mail:</strong> '+email + '</p>')
+}
